59a922e85f05024db0c2482d4aa27c724f0768f5
AMGHAR Nassim <nassimamg@hotmail.com>
2012-11-28 17:13:27 +0100

    instructions compile fusion avec expr

--- s.s
+++ s.s
@@ -1,17 +1,51 @@
 	.text
+f:
+	sub  $sp, $sp, 0
+	sw   $fp, 4($sp)
+	add  $fp, $fp, -4
+	sw   $ra, -4($fp)
+x <= Expression pas encore faite
+ 	lw   $ra, -4($fp)
+	sub  $sp, $sp, 0
+	jr   $ra
 main:
-	sw   $ra, 0($fp)
-	sw   $fp, -4($fp)
-	add  $sp, $fp, -8
+	sub  $sp, $sp, 24
+	sw   $fp, -20($sp)
+	add  $fp, $fp, -4
+	sw   $ra, -4($fp)
 i <= Expression pas encore faite
  k <= 1i <= Expression pas encore faite
+ 1	bnez $a0, True_branch_1
+i <= 1	b    False_branch_1
+True_branch_1:
+i <= 2False_branch_1:
+loop_start_1:
+Expression pas encore faite
+ 	beqz $a0, loop_end_1
+i <= Expression pas encore faite
+  
+ operateur unaire pas encore gere 
+ 	b    loop_start_1
+loop_end_1:
+Expression pas encore faite
+ loop_start_2:
+Expression pas encore faite
+ 	beqz $a0, loop_end_2
+ 
+ operateur unaire pas encore gere 
+ 	b    loop_start_2
+loop_end_2:
+Expression pas encore faite
  chaine <= " 
  
  ma chaine 
  
  "VG <= Expression pas encore faite
-  return qlq chose
- 	.data
+ 	move $a0, $v0
+	lw   $ra, -4($fp)
+	sub  $sp, $sp, 0
+	jr   $ra
+	.data
 i:
 	.word 0
 
